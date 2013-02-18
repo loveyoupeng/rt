92c403ba950c3581a0fe7425232ca1ee1bdd8eae
jgiles <jgiles@localhost>
2013-02-19 10:42:57 +1300

    RT-28337: Regression : many controls cannot be resized

---
+++ javafx-ui-common/test/unit/com/sun/javafx/css/StyleManagerTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sun.javafx.css;
+
+import com.sun.javafx.pgstub.StubToolkit;
+import com.sun.javafx.tk.Toolkit;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dgrieve
+ */
+public class StyleManagerTest {
+    
+    public StyleManagerTest() {
+    }
+    
+    @Before
+    public void setUp() {
+//        rect = new Rectangle();
+//        rect.setId("rectangle");
+//
+//        text = new Text();
+//        text.setId("text");
+//
+//        Group group = new Group();
+//        group.getChildren().addAll(rect, text);
+//
+//        scene = new Scene(group);/* {
+//            TestWindow window;
+//            {
+//                window = new TestWindow();
+//                window.setScene(HonorDeveloperSettingsTest.this.scene);
+//                impl_setWindow(window);
+//            }
+//        };*/
+//        
+//        System.setProperty("binary.css", "false");
+//        String url = getClass().getResource("HonorDeveloperSettingsTest_UA.css").toExternalForm();
+//        StyleManager.getInstance().setDefaultUserAgentStylesheet(url);
+//        
+//        Stage stage = new Stage();
+//        stage.setScene(scene);
+//        stage.show();
+    }
+    
+    @Test
+    public void testMethod_getInstance() {
+        Scene scene = new Scene(new Group());
+        StyleManager sm = StyleManager.getInstance();
+        assertNotNull(sm);
+    }
+        
+}
